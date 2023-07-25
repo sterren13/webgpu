eb8c7181492b5a5b7f9adfbd5b9a313ea29b467b
arno <sterren13@gmail.com>
2023-07-25 11:35:45 +0200

    vertex buffer whit settings

--- src/GPU/GPUVertexBuffer.cpp
+++ src/GPU/GPUVertexBuffer.cpp
@@ -4,6 +4,107 @@
 
 #include "GPUVertexBuffer.h"
 
+BufferElement::BufferElement(DataType type, const std::string &name)
+: Name(name), Type(type), Size(Get_DataTypeSize(type)), ComponentCount(Get_ComponentCount(type)) {}
+
+const uint64_t BufferElement::Get_DataTypeSize(DataType type) {
+    switch (type) {
+        case DataType::Undefined : return 0;
+        case DataType::Uint8x2: return 2;
+        case DataType::Uint8x4: return 4;
+        case DataType::Int8x2: return 2;
+        case DataType::Int8x4: return 4;
+        case DataType::Unorm8x2: return 2;
+        case DataType::Unorm8x4: return 4;
+        case DataType::Norm8x2: return 2;
+        case DataType::Norm8x4: return 4;
+        case DataType::Uint16x2: return 4;
+        case DataType::Uint16x4: return 8;
+        case DataType::Int16x2: return 4;
+        case DataType::Int16x4: return 8;
+        case DataType::Unorm16x2: return 4;
+        case DataType::Unorm16x4: return 8;
+        case DataType::Norm16x2: return 4;
+        case DataType::Norm16x4: return 8;
+        case DataType::Float16x2: return 4;
+        case DataType::Float16x4: return 8;
+        case DataType::Float32: return 4;
+        case DataType::Float32x2: return 8;
+        case DataType::Float32x3: return 12;
+        case DataType::Float32x4: return 16;
+        case DataType::Uint32: return 4;
+        case DataType::Uint32x2: return 8;
+        case DataType::Uint32x3: return 12;
+        case DataType::Uint32x4: return 16;
+        case DataType::Int32: return 4;
+        case DataType::Int32x2: return 8;
+        case DataType::Int32x3: return 12;
+        case DataType::Int32x4: return 16;
+        // TODO case DataType::Force32: return
+    }
+    return 0;
+}
+
+const uint64_t BufferElement::Get_ComponentCount(DataType type) {
+    switch (type) {
+        case DataType::Undefined : return 0;
+        case DataType::Uint8x2: return 2;
+        case DataType::Uint8x4: return 4;
+        case DataType::Int8x2: return 2;
+        case DataType::Int8x4: return 4;
+        case DataType::Unorm8x2: return 2;
+        case DataType::Unorm8x4: return 4;
+        case DataType::Norm8x2: return 2;
+        case DataType::Norm8x4: return 4;
+        case DataType::Uint16x2: return 2;
+        case DataType::Uint16x4: return 4;
+        case DataType::Int16x2: return 2;
+        case DataType::Int16x4: return 4;
+        case DataType::Unorm16x2: return 2;
+        case DataType::Unorm16x4: return 4;
+        case DataType::Norm16x2: return 2;
+        case DataType::Norm16x4: return 4;
+        case DataType::Float16x2: return 2;
+        case DataType::Float16x4: return 4;
+        case DataType::Float32: return 1;
+        case DataType::Float32x2: return 2;
+        case DataType::Float32x3: return 3;
+        case DataType::Float32x4: return 4;
+        case DataType::Uint32: return 1;
+        case DataType::Uint32x2: return 2;
+        case DataType::Uint32x3: return 3;
+        case DataType::Uint32x4: return 4;
+        case DataType::Int32: return 1;
+        case DataType::Int32x2: return 2;
+        case DataType::Int32x3: return 3;
+        case DataType::Int32x4: return 4;
+            // TODO case DataType::Force32: return
+    }
+}
+
+BufferLayout::BufferLayout(std::initializer_list<BufferElement> elements) : BufferElements(elements) {
+    Calculate();
+}
+
+void BufferLayout::PusheLements(std::initializer_list<BufferElement> elements) {
+    for (auto& element : elements)
+        BufferElements.push_back(element);
+    Calculate();
+}
+
+void BufferLayout::Calculate() {
+    size_t offset = 0;
+    Stride = 0;
+    BufferAttributes.clear();
+    for (auto& element : BufferElements){
+        WGPUVertexAttribute attribute;
+        attribute.offset = offset;
+        attribute.format = ;
+
+        BufferAttributes.push_back()
+    }
+}
+
 GPUVertexBuffer::GPUVertexBuffer(GPUDevice& device, uint64_t VertexCount) : GPUBuffer(device, VertexCount*2*sizeof(float), CopyDst | Vertex) {
     // TODO auto gen
     vertexAttrib.shaderLocation = 0;
--- src/GPU/GPUVertexBuffer.h
+++ src/GPU/GPUVertexBuffer.h
@@ -6,6 +6,64 @@
 #define TESTWEBGPU_GPUVERTEXBUFFER_H
 #include "GPUBuffer.h"
 #include "GPUDevice.h"
+#include <string>
+#include <initializer_list>
+#include <vector>
+
+enum class DataType {
+//  vec 1       vec 2       vec 3       vec 4
+    Undefined,
+                Uint8x2,                Uint8x4,
+                Int8x2,                 Int8x4,
+                Unorm8x2,               Unorm8x4,
+                Norm8x2,                Norm8x4,
+                Uint16x2,               Uint16x4,
+                Int16x2,                Int16x4,
+                Unorm16x2,              Unorm16x4,
+                Norm16x2,               Norm16x4,
+                Float16x2,              Float16x4,
+    Float32,    Float32x2,  Float32x3,  Float32x4,
+    Uint32,     Uint32x2,   Uint32x3,   Uint32x4,
+    Int32,      Int32x2,    Int32x3,    Int32x4,
+    Force32
+};
+
+class BufferElement {
+public:
+    BufferElement() = default;
+    BufferElement(DataType type, const std::string& name);
+
+    inline void SetName(std::string& name) {Name = name;}
+    inline void SetType(DataType type) {Type = type; Size = Get_DataTypeSize(type);}
+
+    [[nodiscard]] inline std::string GetName() const{return Name;}
+    [[nodiscard]] inline DataType GetType() const{return Type;}
+    [[nodiscard]] inline uint64_t GetSize() const{return Size;}
+    [[nodiscard]] inline uint64_t GetComponentCount() const{return ComponentCount;}
+private:
+    static const uint64_t Get_DataTypeSize(DataType type);
+    static const uint64_t Get_ComponentCount(DataType type);
+private:
+    std::string Name = "";
+    DataType Type = DataType::Undefined;
+    uint64_t Size = 0;
+    uint64_t ComponentCount = 0;
+};
+
+class BufferLayout {
+public:
+    BufferLayout() = default;
+    BufferLayout(std::initializer_list<BufferElement> elements);
+
+    [[nodiscard]] inline size_t GetStride() const {return Stride;}
+    void PusheLements(std::initializer_list<BufferElement> elements);
+
+private:
+    void Calculate();
+    std::vector<BufferElement> BufferElements;
+    std::vector<WGPUVertexAttribute> BufferAttributes;
+    uint64_t Stride = 0;
+};
 
 class GPUVertexBuffer : public GPUBuffer {
 public:
@@ -18,6 +76,5 @@ private:
     WGPUVertexAttribute vertexAttrib;
     WGPUVertexBufferLayout vertexBufferLayout{};
 };
-
 
 #endif //TESTWEBGPU_GPUVERTEXBUFFER_H
